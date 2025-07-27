5e34c79b07e50f9fdf5a872e21d609932d8999cf
GauravBhendekar <bhendekargaurav2004@gmail.com>
2025-07-27 23:36:01 +0530

    Update login.js

--- login.js
+++ login.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
 
       try {
-        const response = await fetch('http://localhost:3000/login', {
+        const response = await fetch('https://vendorkart.onrender.com', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
