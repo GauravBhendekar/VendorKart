93d485694b2f7d1e1fb92ba1bc8ca4d57eb142d2
GauravBhendekar <bhendekargaurav2004@gmail.com>
2025-07-27 23:53:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if (form) {
 
     const formData = new FormData(form);
 
-    fetch("https://oidc.vercel.com/falcon-9a68a5aa", {
+    fetch("https://vendorkart.onrender.com", {
       method: "POST",
       body: formData,
     })
